f89ecdf94aa68d30d9d3deae4d4e9e79ebe55f67
James <engrjayt200@gmail.com>
2022-11-08 16:22:36 +0100

    Add verify phone Page

--- client/src/components/PhoneVerify.jsx
+++ client/src/components/PhoneVerify.jsx
@@ -16,7 +16,19 @@ const PhoneVerify = () => {
   return (
     <div className='verify'>
       <h2 style={{marginBottom: "30px"}}>Verify your Phone number</h2>
-
+       <form className='verify__form' onSubmit={handleSubmit}>
+        <label htmlFor='code' style={{marginBottom:"10px"}}> A  code has been sent to your phone</label>
+        <input 
+        type='text'
+        name='code'
+        className='code'
+        id='code'
+        value={code}
+        onChange = {(e) => setCode(e.target.value)}
+        require 
+        />
+        <button className='codeBtn'>AUTHENTICATE</button>
+       </form>
     </div>
   )
 }
